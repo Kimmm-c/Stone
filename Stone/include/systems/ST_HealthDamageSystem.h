c3bbbc84b84e9dca955707a6b96b11e43c9d0320
kapybara <97310663+Kimmm-c@users.noreply.github.com>
2026-04-09 12:56:52 -0700

    added design doc and README

--- Stone/include/systems/ST_HealthDamageSystem.h
+++ Stone/include/systems/ST_HealthDamageSystem.h
@@ -18,7 +18,7 @@ public:
                 if (acc.targetPlayer && acc.targetPlayer->hasComponent<Health>()) {
 
                     // TODO: replace hardcoded ranges
-                    ST_Vector2D overlappingRange{ 0.0f, 32.0f * 32.0f };
+                    ST_Vector2D overlappingRange{ 0.0f, 32.0f * 32.0f }; // 32x32 is the projectile area
                     ST_Vector2D healthRange{ 0.0f, 5000.0f };
 
                     float overlap = std::min( acc.maxOverlap, overlappingRange.y );
